53d95e26f791499dbeed2e18caede3f769440a29
Kirill Bulatov <mail4score@gmail.com>
2018-11-07 15:38:41 +0200

    Initial service metadata read.

---
+++ vaadin-services-demo/src/main/java/com/vaadin/services/TestService.java
@@ -0,0 +1,9 @@
+package com.vaadin.services;
+
+@VaadinService
+public class TestService {
+
+    public String test() {
+        return "test successful";
+    }
+}
---
+++ vaadin-services/src/main/java/com/vaadin/services/VaadinService.java
@@ -0,0 +1,15 @@
+package com.vaadin.services;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface VaadinService {
+    String serviceName() default "";
+}
