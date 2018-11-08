5d5ee49b6294b4bd52bd83dddfb8e61b765ea8d1
Kirill Bulatov <mail4score@gmail.com>
2018-11-08 15:29:14 +0200

    Reformat the code.

--- vaadin-services-demo/src/main/java/com/vaadin/services/TestService.java
+++ vaadin-services-demo/src/main/java/com/vaadin/services/TestService.java
@@ -10,32 +10,31 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @VaadinService
 public class TestService {
 
-    public String test() {
-        return "test successful";
+  public String test() {
+    return "test successful";
+  }
+
+  public Map<String, List<TestObject>> complexTest(int count) {
+    Map<String, List<TestObject>> result = new HashMap<>(count, 1);
+    for (int i = 0; i < count; i++) {
+      List<TestObject> inner = new ArrayList<>(count);
+      for (int j = 0; j < i; j++) {
+        inner.add(new TestObject(j));
+      }
+      result.put(Integer.toString(i), inner);
     }
+    return result;
+  }
 
-    public Map<String, List<TestObject>> complexTest(int count) {
-        Map<String, List<TestObject>> result = new HashMap<>(count, 1);
-        for (int i = 0; i < count; i++) {
-            List<TestObject> inner = new ArrayList<>(count);
-            for (int j = 0; j < i; j++) {
-                inner.add(new TestObject(j));
-            }
-            result.put(Integer.toString(i), inner);
-        }
-        return result;
+  public static class TestObject {
+    private final int number;
+
+    @JsonIgnore
+    private final int number2;
+
+    public TestObject(int number) {
+      this.number = number;
+      this.number2 = number;
     }
-
-
-    public static class TestObject {
-        private final int number;
-
-        @JsonIgnore
-        private final int number2;
-
-        public TestObject(int number) {
-            this.number = number;
-            this.number2 = number;
-        }
-    }
+  }
 }
--- vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
+++ vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
@@ -29,94 +30,112 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VaadinServiceController {
-    private final ObjectMapper vaadinServiceMapper;
-    private final Map<String, VaadinServiceData> vaadinServices = new HashMap<>();
+  private final ObjectMapper vaadinServiceMapper;
+  private final Map<String, VaadinServiceData> vaadinServices = new HashMap<>();
 
-    private static class VaadinServiceData {
-        private final Object vaadinServiceObject;
-        private final Map<String, Method> methods = new HashMap<>();
+  private static class VaadinServiceData {
+    private final Object vaadinServiceObject;
+    private final Map<String, Method> methods = new HashMap<>();
 
-        private VaadinServiceData(Object vaadinServiceObject, Method... serviceMethods) {
-            this.vaadinServiceObject = vaadinServiceObject;
-            Stream.of(serviceMethods)
-                    .filter(method -> method.getDeclaringClass() != Object.class)
-                    .forEach(method -> methods.put(method.getName().toLowerCase(Locale.ENGLISH), method));
-        }
-
-        private Optional<Method> getMethod(String methodName) {
-            return Optional.ofNullable(methods.get(methodName));
-        }
-
-        private Object getServiceObject() {
-            return vaadinServiceObject;
-        }
+    private VaadinServiceData(Object vaadinServiceObject,
+        Method... serviceMethods) {
+      this.vaadinServiceObject = vaadinServiceObject;
+      Stream.of(serviceMethods)
+          .filter(method -> method.getDeclaringClass() != Object.class)
+          .forEach(method -> methods
+              .put(method.getName().toLowerCase(Locale.ENGLISH), method));
     }
 
-    public VaadinServiceController(
-            @Autowired(required = false) @Qualifier("vaadinServiceMapper") ObjectMapper vaadinServiceMapper,
-            ApplicationContext context
-    ) {
-        this.vaadinServiceMapper = vaadinServiceMapper != null ? vaadinServiceMapper : Jackson2ObjectMapperBuilder
-                .json().visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY).build();
+    private Optional<Method> getMethod(String methodName) {
+      return Optional.ofNullable(methods.get(methodName));
+    }
 
-        context.getBeansWithAnnotation(VaadinService.class).forEach((name, serviceBean) -> {
-            // Check the bean type instead of the implementation type in
-            // case of e.g. proxies
-            Class<?> beanType = context.getType(name);
-            if (beanType == null) {
-                throw new IllegalStateException("WTF?");
-            }
+    private Object getServiceObject() {
+      return vaadinServiceObject;
+    }
+  }
 
-            String serviceName = beanType.getSimpleName();
-            if (serviceName.isEmpty()) {
-                throw new IllegalStateException("Anonymous class as a VaadinService declared, wtf?");
-            }
+  public VaadinServiceController(
+      @Autowired(required = false) @Qualifier("vaadinServiceMapper") ObjectMapper vaadinServiceMapper,
+      ApplicationContext context) {
+    this.vaadinServiceMapper = vaadinServiceMapper != null ? vaadinServiceMapper
+        : Jackson2ObjectMapperBuilder.json()
+            .visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+            .build();
 
-            vaadinServices.put(serviceName.toLowerCase(Locale.ENGLISH), new VaadinServiceData(serviceBean, beanType.getMethods()));
+    context.getBeansWithAnnotation(VaadinService.class)
+        .forEach((name, serviceBean) -> {
+          // Check the bean type instead of the implementation type in
+          // case of e.g. proxies
+          Class<?> beanType = context.getType(name);
+          if (beanType == null) {
+            throw new IllegalStateException("WTF?");
+          }
+
+          String serviceName = beanType.getSimpleName();
+          if (serviceName.isEmpty()) {
+            throw new IllegalStateException(
+                "Anonymous class as a VaadinService declared, wtf?");
+          }
+
+          vaadinServices.put(serviceName.toLowerCase(Locale.ENGLISH),
+              new VaadinServiceData(serviceBean, beanType.getMethods()));
         });
+  }
+
+  // @formatter:off
+  // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/ser/var
+  // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/testservice/ss
+
+  // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/testservice/test
+  // curl -i -H "Content-Type: application/json" -d '{"count":3}' http://localhost:8080/testservice/complexTest
+  // @formatter:on
+  @PostMapping(path = "/{service}/{method}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<String> serveVaadinService(
+      @PathVariable("service") String serviceName,
+      @PathVariable("method") String methodName,
+      @RequestBody(required = false) ObjectNode body)
+      throws IOException, InvocationTargetException, IllegalAccessException {
+    VaadinServiceData vaadinServiceData = vaadinServices
+        .get(serviceName.toLowerCase(Locale.ENGLISH));
+    Method methodToInvoke = vaadinServiceData == null ? null
+        : vaadinServiceData.getMethod(methodName.toLowerCase(Locale.ENGLISH))
+            .orElse(null);
+    if (methodToInvoke == null) {
+      return ResponseEntity.notFound().build();
     }
 
-    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/ser/var
-    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/testservice/ss
+    Object returnValue = methodToInvoke.invoke(
+        vaadinServiceData.getServiceObject(),
+        getVaadinServiceParameters(body, methodToInvoke.getParameters()));
+    return ResponseEntity
+        .ok(vaadinServiceMapper.writeValueAsString(returnValue));
+  }
 
-    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/testservice/test
-    // curl -i -H "Content-Type: application/json" -d '{"count":3}' http://localhost:8080/testservice/complexTest
-    @PostMapping(path = "/{service}/{method}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String> serveVaadinService(@PathVariable("service") String serviceName,
-                                                     @PathVariable("method") String methodName,
-                                                     @RequestBody(required = false) ObjectNode body) throws IOException, InvocationTargetException, IllegalAccessException {
-        VaadinServiceData vaadinServiceData = vaadinServices.get(serviceName.toLowerCase(Locale.ENGLISH));
-        Method methodToInvoke = vaadinServiceData == null ? null : vaadinServiceData.getMethod(methodName.toLowerCase(Locale.ENGLISH)).orElse(null);
-        if (methodToInvoke == null) {
-            return ResponseEntity.notFound().build();
-        }
+  private Object[] getVaadinServiceParameters(
+      @RequestBody(required = false) ObjectNode body,
+      Parameter[] javaParameters) throws IOException {
+    List<JsonNode> requestParameters = getRequestParameters(body);
+    Object[] javaArguments = new Object[javaParameters.length];
+    int nextJsonParamIndex = 0;
+    for (int i = 0; i < javaParameters.length; i++) {
+      Parameter parameter = javaParameters[i];
+      Object value;
 
-        Map<String, JsonNode> parametersData = getParametersData(body);
-        ArrayList<JsonNode> paramsInOrder = new ArrayList<>(parametersData.values());
-        Parameter[] javaParameters = methodToInvoke.getParameters();
-        Object[] javaArguments = new Object[javaParameters.length];
-        int nextJsonParamIndex = 0;
-        for (int i = 0; i < javaParameters.length; i++) {
-            Parameter parameter = javaParameters[i];
-            Object value;
-
-            JsonNode jsonValue = paramsInOrder
-                    .get(nextJsonParamIndex++);
-            value = vaadinServiceMapper.readerFor(parameter.getType())
-                    .readValue(jsonValue);
-            javaArguments[i] = value;
-        }
-
-        Object returnValue = methodToInvoke
-                .invoke(vaadinServiceData.getServiceObject(), javaArguments);
-        return ResponseEntity.ok(vaadinServiceMapper.writeValueAsString(returnValue));
+      JsonNode jsonValue = requestParameters.get(nextJsonParamIndex++);
+      value = vaadinServiceMapper.readerFor(parameter.getType())
+          .readValue(jsonValue);
+      javaArguments[i] = value;
     }
+    return javaArguments;
+  }
 
-    private Map<String, JsonNode> getParametersData(ObjectNode body) {
-        Map<String, JsonNode> parametersData = new HashMap<>();
-        if (body != null) {
-            body.fields().forEachRemaining(entry -> parametersData.put(entry.getKey(), entry.getValue()));
-        }
-        return parametersData;
+  private List<JsonNode> getRequestParameters(ObjectNode body) {
+    List<JsonNode> parametersData = new ArrayList<>();
+    if (body != null) {
+      body.fields()
+          .forEachRemaining(entry -> parametersData.add(entry.getValue()));
     }
+    return parametersData;
+  }
 }
