8b264c6f5be84daeb05a9a7f2ec8428b4c8216e6
Kirill Bulatov <mail4score@gmail.com>
2018-11-08 11:23:25 +0200

    Fix complex serialization issues.

--- vaadin-services-demo/src/main/java/com/vaadin/services/TestService.java
+++ vaadin-services-demo/src/main/java/com/vaadin/services/TestService.java
@@ -1,9 +1,41 @@
 package com.vaadin.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @VaadinService
 public class TestService {
 
     public String test() {
         return "test successful";
+    }
+
+    public Map<String, List<TestObject>> complexTest(int count) {
+        Map<String, List<TestObject>> result = new HashMap<>(count, 1);
+        for (int i = 0; i < count; i++) {
+            List<TestObject> inner = new ArrayList<>(count);
+            for (int j = 0; j < i; j++) {
+                inner.add(new TestObject(j));
+            }
+            result.put(Integer.toString(i), inner);
+        }
+        return result;
+    }
+
+
+    public static class TestObject {
+        private final int number;
+
+        @JsonIgnore
+        private final int number2;
+
+        public TestObject(int number) {
+            this.number = number;
+            this.number2 = number;
+        }
     }
 }
