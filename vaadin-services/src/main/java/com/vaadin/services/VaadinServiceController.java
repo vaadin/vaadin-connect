7d55c11b329615a8c48c82abba027fe528976e62
Kirill Bulatov <mail4score@gmail.com>
2018-11-08 10:39:55 +0200

    Use lower case for both service and method name location.

--- vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
+++ vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
@@ -2,12 +2,15 @@ package com.vaadin.services;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +27,11 @@ public class VaadinServiceController {
             this.vaadinServiceClass = vaadinServiceClass;
             Stream.of(serviceMethods)
                     .filter(method -> method.getDeclaringClass() != Object.class)
-                    .forEach(method -> methods.put(method.getName(), method));
+                    .forEach(method -> methods.put(method.getName().toLowerCase(Locale.ENGLISH), method));
+        }
+
+        private Optional<Method> getMethod(String methodName) {
+            return Optional.ofNullable(methods.get(methodName));
         }
     }
 
@@ -43,18 +50,21 @@ public class VaadinServiceController {
                 throw new IllegalStateException("Anonymous class as a VaadinService declared, wtf?");
             }
 
-            vaadinServices.put(serviceName, new VaadinServiceData(serviceBean, beanType.getMethods()));
+            vaadinServices.put(serviceName.toLowerCase(Locale.ENGLISH), new VaadinServiceData(serviceBean, beanType.getMethods()));
         });
-
-        System.out.println("=======");
-        System.out.println(vaadinServices.keySet());
-        System.out.println("=======");
     }
 
-    // curl -H "Content-Type: application/json" -d "" http://localhost:8080/ser/var
+    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/ser/var
+    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/testservice/ss
+    // curl -i -H "Content-Type: application/json" -d "" http://localhost:8080/testservice/test
     @PostMapping(path = "/{service}/{method}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String serveVaadinService(@PathVariable("service") String serviceName,
-                                     @PathVariable("method") String methodName) {
-        return "Service stub";
+    public ResponseEntity<String> serveVaadinService(@PathVariable("service") String serviceName,
+                                                     @PathVariable("method") String methodName) {
+        VaadinServiceData vaadinServiceData = vaadinServices.get(serviceName.toLowerCase(Locale.ENGLISH));
+        Method methodToInvoke = vaadinServiceData == null ? null : vaadinServiceData.getMethod(methodName.toLowerCase(Locale.ENGLISH)).orElse(null);
+        if (methodToInvoke == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok("Service stub");
     }
 }
