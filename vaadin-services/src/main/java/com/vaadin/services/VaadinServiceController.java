4f5f4133dfd89fa328a027817d08c8adf6374a82
Kirill Bulatov <mail4score@gmail.com>
2018-11-08 16:28:14 +0200

    Process exceptions properly.

--- vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
+++ vaadin-services/src/main/java/com/vaadin/services/VaadinServiceController.java
@@ -10,16 +10,19 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -91,7 +94,8 @@ public class VaadinServiceController {
   // @formatter:off
   // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/ser/var
   // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/testservice/ss
-  // curl -i -H "Content-Type: application/json" -d '{"testParam": ["test", 5]}' http://localhost:8080/testservice/noReturnValue
+  // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/testservice/complexTest
+  // curl -i -H "Content-Type: application/json" -d '{"arg1": [1], "arg2": 2, "arg3": 3.0}' http://localhost:8080/testservice/testMultipleParameters
 
   // curl -i -H "Content-Type: application/json" -d '' http://localhost:8080/testservice/test
   // curl -i -H "Content-Type: application/json" -d '{"count": 3}' http://localhost:8080/testservice/complexTest
@@ -103,7 +107,9 @@ public class VaadinServiceController {
       @PathVariable("service") String serviceName,
       @PathVariable("method") String methodName,
       @RequestBody(required = false) ObjectNode body)
-      throws IOException, InvocationTargetException, IllegalAccessException {
+  // TODO kb process this exception also
+  // TODO kb do not show stacktraces by default in response messages
+      throws InvocationTargetException {
     VaadinServiceData vaadinServiceData = vaadinServices
         .get(serviceName.toLowerCase(Locale.ENGLISH));
     Method methodToInvoke = vaadinServiceData == null ? null
@@ -113,17 +119,49 @@ public class VaadinServiceController {
       return ResponseEntity.notFound().build();
     }
 
-    Object returnValue = methodToInvoke.invoke(
-        vaadinServiceData.getServiceObject(),
-        getVaadinServiceParameters(body, methodToInvoke.getParameters()));
-    return ResponseEntity
-        .ok(vaadinServiceMapper.writeValueAsString(returnValue));
+    List<JsonNode> requestParameters = getRequestParameters(body);
+    Parameter[] javaParameters = methodToInvoke.getParameters();
+    if (javaParameters.length != requestParameters.size()) {
+      return ResponseEntity.badRequest().body(String.format(
+          "Incorrect number of parameters for service '%s' method '%s', expected: '%s', got: '%s'",
+          serviceName, methodName, javaParameters.length,
+          requestParameters.size()));
+    }
+
+    Object[] vaadinServiceParameters;
+    try {
+      vaadinServiceParameters = getVaadinServiceParameters(requestParameters,
+          javaParameters);
+    } catch (IOException e) {
+      return ResponseEntity.badRequest().body(String.format(
+          "Unable to deserialize parameters for service '%s' method '%s'. Expected parameter types (and their order) are: '[%s]'",
+          serviceName, methodName,
+          Stream.of(javaParameters).map(Parameter::getType).map(Class::getName)
+              .collect(Collectors.joining(", "))));
+    }
+
+    Object returnValue;
+    try {
+      returnValue = methodToInvoke
+          .invoke(vaadinServiceData.getServiceObject(), vaadinServiceParameters);
+    } catch (IllegalAccessException e) {
+      // TODO kb return a proper error code instead
+      throw new IllegalStateException(e);
+    }
+
+    try {
+      return ResponseEntity
+          .ok(vaadinServiceMapper.writeValueAsString(returnValue));
+    } catch (JsonProcessingException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(String.format(
+              "Failed to serialize service '%s' method '%s' response. Double check method's return type or specify a custom mapper bean with qualifier '%s'",
+              serviceName, methodName, VAADIN_SERVICE_MAPPER_BEAN_QUALIFIER));
+    }
   }
 
-  private Object[] getVaadinServiceParameters(
-      @RequestBody(required = false) ObjectNode body,
+  private Object[] getVaadinServiceParameters(List<JsonNode> requestParameters,
       Parameter[] javaParameters) throws IOException {
-    List<JsonNode> requestParameters = getRequestParameters(body);
     Object[] serviceParameters = new Object[javaParameters.length];
     for (int i = 0; i < javaParameters.length; i++) {
       serviceParameters[i] = vaadinServiceMapper
