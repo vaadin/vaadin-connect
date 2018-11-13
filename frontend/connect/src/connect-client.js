4ed332db671b9567ad3691cc801091ea172179e2
Anton Platonov <platosha@gmail.com>
2018-11-13 18:49:04 +0200

    feat(frontend): add ConnectClient JS client class
    Fixes #2

---
+++ frontend/connect/src/connect-client.js
@@ -0,0 +1,66 @@
+/**
+ * Vaadin Connect client class is a low-level network calling utility. It stores
+ * an endpoint and facilitates remote calls to services and methods
+ * on the Vaadin Connect backend.
+ *
+ * ```js
+ * const client = new ConnectClient();
+ * const responseData = await client.call('MyVaadinService', 'myMethod');
+ * ```
+ *
+ * Supports an `endpoint` constructor option:
+ * ```js
+ * const client = new ConnectClient({endpoint: '/my-connect-endpont'});
+ * ```
+ *
+ * The default endpoint is '/connect'.
+ */
+export class ConnectClient {
+  constructor(options = {}) {
+    /**
+     * @property {string} endpoint="/connect" The Vaadin Connect backend
+     * endpoint.
+     */
+    this.endpoint = options.endpoint || '/connect';
+  }
+
+  /**
+   * Makes a JSON HTTP request to the `${endpoint}/${service}/${method}` URL,
+   * optionally supplying the provided params as a JSON request body,
+   * and asynchronously returns the parsed JSON response data.
+   *
+   * @param {string} service Service class name.
+   * @param {string} method Method name to call in the service class.
+   * @param {Object=} params Optional object to be send in JSON request body.
+   * @return {Promise<Object>} Decoded JSON response data.
+   */
+  async call(service, method, params) {
+    if (arguments.length < 2) {
+      throw new TypeError(
+        `2 arguments required, but got only ${arguments.length}`
+      );
+    }
+
+    /* global fetch */
+    const response = await fetch(
+      `${this.endpoint}/${service}/${method}`,
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: params !== undefined ? JSON.stringify(params) : undefined
+      }
+    );
+
+    if (!response.ok) {
+      throw new TypeError(
+        `expected '200 OK' response, but got ${response.status}`
+        + ` ${response.statusText}`
+      );
+    }
+
+    return response.json();
+  }
+}
