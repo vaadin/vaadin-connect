ab9eb4d54707891b6a2dde37c32ab27d6e867500
Kirill Bulatov <mail4score@gmail.com>
2018-11-17 20:15:21 +0200

    Fix oauth configuration

--- vaadin-connect/src/main/java/com/vaadin/connect/oauth/VaadinConnectOAuthConfigurer.java
+++ vaadin-connect/src/main/java/com/vaadin/connect/oauth/VaadinConnectOAuthConfigurer.java
@@ -13,119 +13,58 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.vaadin.connect.oauth;
 
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.stereotype.Component;
 
 import com.vaadin.connect.VaadinConnectProperties;
 
-/**
- * Class to configure the authentication of a vaadin-connect application
- *
- * Configure oauth by annotating your app with the
- * {@link EnableVaadinConnectOAuthServer} and defining either a
- * {@link UserDetailsService} or a {@link AuthenticationManager} Bean.
- *
- * <pre class="code">
- * &#64;Configuration
- * public class MyApplicationConfiguration {
- *   &#64;Autowired
- *   private AccountRepository accountRepository;
- *
- *   &#64;Bean
- *   public UserDetailsService userDetailsService() {
- *     return username -> this.accountRepository
- *         .findByUsername(username)
- *         .map(account -> User.builder()
- *             .username(account.getUsername())
- *             .password(account.getPassword())
- *             .roles("USER")
- *             .build())
- *         .orElseThrow(() -> new UsernameNotFoundException(username));
- *   }
- * }
- *
- * &#64;Configuration
- * public class MyApplicationConfiguration {
- *   &#64;Bean
- *   AuthenticationManager authenticationManager() {
- *     return new AuthenticationManager() {
- *       &#64;Override
- *       public Authentication authenticate(Authentication auth)
- *           throws AuthenticationException {
- *
- *         return new UsernamePasswordAuthenticationToken(
- *             auth.getName(), auth.getCredentials(), new ArrayList<>());
- *       }
- *     };
- *   }
- * }
- * </pre>
- *
- * This configurator automatically register a {@link BCryptPasswordEncoder}. If
- * you store passwords in your database using another encoding algorithm define
- * your own {@link PasswordEncoder} Bean.
- *
- * <pre class="code">
- * public class MyApplicationConfiguration {
- *   &#64;Bean
- *   public PasswordEncoder passwordEncoder() {
- *     // When using plain passwords stored in user database
- *     return NoOpPasswordEncoder.getInstance();
- *   }
- * }
- * </pre>
- */
-@Import({ VaadinConnectProperties.class })
+// TODO javadocs to prevent Sonar rants
+
 @Component
 public class VaadinConnectOAuthConfigurer
     extends AuthorizationServerConfigurerAdapter {
-
-  private static final String DEFAULT_SIGNING_KEY = "JustAnySigningK3y";
   private static final String[] SCOPES = new String[] { "read", "write" };
-  private static final String[] GRANT_TYPES =
-      new String[] { "password", "refresh_token" };
+  private static final String[] GRANT_TYPES = new String[] { "password",
+      "refresh_token" };
 
-  @Autowired
-  private AuthenticationConfiguration authenticationConfiguration;
+  private final PasswordEncoder encoder;
+  private final VaadinConnectProperties vaadinConnectProperties;
+  private final TokenStore tokenStore;
+  private final JwtAccessTokenConverter accessTokenConverter;
+  private final AuthenticationConfiguration authenticationConfiguration;
+  private final ApplicationContext applicationContext;
 
-  @Autowired
-  private PasswordEncoder encoder;
-
-  @Autowired
-  private VaadinConnectProperties vaadinConnectProperties;
-
-  @Autowired
-  private ApplicationContext applicationContext;
-
-  @Autowired
-  private JwtAccessTokenConverter accessTokenConverter;
+  public VaadinConnectOAuthConfigurer(PasswordEncoder encoder,
+      VaadinConnectProperties vaadinConnectProperties, TokenStore tokenStore,
+      JwtAccessTokenConverter accessTokenConverter,
+      AuthenticationConfiguration authenticationConfiguration,
+      ApplicationContext applicationContext) {
+    this.encoder = encoder;
+    this.vaadinConnectProperties = vaadinConnectProperties;
+    this.tokenStore = tokenStore;
+    this.accessTokenConverter = accessTokenConverter;
+    this.authenticationConfiguration = authenticationConfiguration;
+    this.applicationContext = applicationContext;
+  }
 
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints)
       throws Exception {
     // This is required for 'password' grants, which is specified below
     endpoints.authenticationManager(authenticationManager())
-        .tokenStore(tokenStore())
-        .accessTokenConverter(accessTokenConverter);
+        .tokenStore(tokenStore).accessTokenConverter(accessTokenConverter);
   }
 
   @Override
@@ -135,26 +74,7 @@ public class VaadinConnectOAuthConfigurer
         .withClient(vaadinConnectProperties.getVaadinConnectClientAppname())
         .secret(encoder
             .encode(vaadinConnectProperties.getVaadinConnectClientSecret()))
-        .scopes(SCOPES)
-        .authorizedGrantTypes(GRANT_TYPES);
-  }
-
-  /**
-   * @return the JwtAccessTokenConverter
-   */
-  @Bean
-  public JwtAccessTokenConverter accessTokenConverter() {
-    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-    converter.setSigningKey(DEFAULT_SIGNING_KEY);
-    return converter;
-  }
-
-  /**
-   * @return the TokenStore
-   */
-  @Bean
-  public TokenStore tokenStore() {
-    return new JwtTokenStore(accessTokenConverter);
+        .scopes(SCOPES).authorizedGrantTypes(GRANT_TYPES);
   }
 
   private AuthenticationManager authenticationManager() throws Exception {
@@ -165,12 +85,4 @@ public class VaadinConnectOAuthConfigurer
     }
   }
 
-  @Configuration
-  @ConditionalOnMissingBean(PasswordEncoder.class)
-  protected static class PasswordEncoderConfiguration {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
-  }
 }
--- vaadin-connect/src/test/java/com/vaadin/connect/oauth/VaadinConnectOAuthConfigurerTest.java
+++ vaadin-connect/src/test/java/com/vaadin/connect/oauth/VaadinConnectOAuthConfigurerTest.java
@@ -1,14 +1,5 @@
 package com.vaadin.connect.oauth;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -23,6 +14,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
@@ -38,6 +30,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.vaadin.connect.VaadinConnectProperties;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * This test is inspired in the pattern used in
@@ -156,6 +159,7 @@ public class VaadinConnectOAuthConfigurerTest {
   @Configuration
   @EnableVaadinConnectOAuthServer
   @EnableWebMvc
+  @Import(VaadinConnectProperties.class)
   protected static class EnableVaadinOauth {
   }
 
