3b245995baf3c5c8228974e618242f373b4d6503
Tien Nguyen <nqtien.dev@gmail.com>
2018-11-19 13:09:38 +0200

    Add test for invalid token

--- vaadin-connect-demo/src/test/java/com/vaadin/connect/demo/VaadinConnectOauthTokenIT.java
+++ vaadin-connect-demo/src/test/java/com/vaadin/connect/demo/VaadinConnectOauthTokenIT.java
@@ -15,6 +15,26 @@
  */
 package com.vaadin.connect.demo;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
+import org.springframework.security.oauth2.common.util.JacksonJsonParser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
 import static com.vaadin.connect.demo.DemoVaadinOAuthConfiguration.TEST_LOGIN;
 import static com.vaadin.connect.demo.DemoVaadinOAuthConfiguration.TEST_PASSWORD;
 import static org.junit.Assert.assertEquals;
@@ -24,19 +44,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.oauth2.common.util.JacksonJsonParser;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
@@ -101,5 +108,95 @@ public class VaadinConnectOauthTokenIT {
     mockMvc.perform(get("/hello")
         .header("Authorization", "Bearer " + accessToken))
         .andExpect(content().string("Hello Word"));
+  }
+
+  @Test
+  public void should_ReturnUnauthorized_When_TokenIsExpired() throws Exception {
+    String resultString = getToken(TEST_LOGIN, TEST_PASSWORD).andReturn()
+        .getResponse().getContentAsString();
+
+    String accessToken = (String) parser.parseMap(resultString)
+        .get("access_token");
+    String[] accessTokenParts = accessToken.split("\\.");
+
+    JacksonJsonParser parser = new JacksonJsonParser();
+
+    Map<String, Object> claims = parser.parseMap(
+        new String(Base64Utils.decodeFromString(accessTokenParts[1])));
+    claims.put("exp",
+        Instant.now().minus(1, ChronoUnit.MINUTES).getEpochSecond());
+    String expiredAccessToken = generateNewToken(parser.formatMap(claims),
+        "JustAnySigningK3y");
+
+    mockMvc.perform(
+        get("/hello").header("Authorization", "Bearer " + expiredAccessToken))
+        .andExpect(status().is(401));
+  }
+
+  @Test
+  public void should_ReturnUnauthorized_When_TokenHasNullClaim()
+      throws Exception {
+    String resultString = getToken(TEST_LOGIN, TEST_PASSWORD).andReturn()
+        .getResponse().getContentAsString();
+
+    String accessToken = (String) parser.parseMap(resultString)
+        .get("access_token");
+    String[] accessTokenParts = accessToken.split("\\.");
+
+    JacksonJsonParser parser = new JacksonJsonParser();
+
+    Map<String, Object> claims = parser.parseMap(
+        new String(Base64Utils.decodeFromString(accessTokenParts[1])));
+    claims.put("user_name", null);
+    String expiredAccessToken = generateNewToken(parser.formatMap(claims),
+        "JustAnySigningK3y");
+
+    mockMvc.perform(
+        get("/hello").header("Authorization", "Bearer " + expiredAccessToken))
+        .andExpect(status().is(401));
+  }
+
+  @Test
+  public void should_ReturnUnauthorized_When_TokenMissedARequiredClaim()
+      throws Exception {
+    String resultString = getToken(TEST_LOGIN, TEST_PASSWORD).andReturn()
+        .getResponse().getContentAsString();
+
+    String accessToken = (String) parser.parseMap(resultString)
+        .get("access_token");
+    String[] accessTokenParts = accessToken.split("\\.");
+
+    JacksonJsonParser parser = new JacksonJsonParser();
+
+    Map<String, Object> claims = parser.parseMap(
+        new String(Base64Utils.decodeFromString(accessTokenParts[1])));
+    claims.remove("user_name");
+    String expiredAccessToken = generateNewToken(parser.formatMap(claims),
+        "JustAnySigningK3y");
+
+    mockMvc.perform(
+        get("/hello").header("Authorization", "Bearer " + expiredAccessToken))
+        .andExpect(status().is(401));
+  }
+
+  @Test
+  public void should_ReturnUnauthorized_When_TokenIsSignedWithDifferentSigningKey()
+      throws Exception {
+    String resultString = getToken(TEST_LOGIN, TEST_PASSWORD).andReturn()
+        .getResponse().getContentAsString();
+
+    String accessToken = (String) parser.parseMap(resultString)
+        .get("access_token");
+    String[] accessTokenParts = accessToken.split("\\.");
+    String expiredAccessToken = generateNewToken(accessTokenParts[1],
+        "DifferentSigningKey");
+    mockMvc.perform(
+        get("/hello").header("Authorization", "Bearer " + expiredAccessToken))
+        .andExpect(status().is(401));
+  }
+
+  private String generateNewToken(String claims, String signingKey) {
+    MacSigner defaultMacSigner = new MacSigner(signingKey);
+    return JwtHelper.encode(claims, defaultMacSigner).getEncoded();
   }
 }
