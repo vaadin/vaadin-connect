2f5c7ba24c305a5024e88a24fb5b31119339ff2b
Kirill Bulatov <mail4score@gmail.com>
2018-11-30 13:19:00 +0200

    Add direct Vaadin Connect client usage example

--- vaadin-connect-demo/frontend/index.js
+++ vaadin-connect-demo/frontend/index.js
@@ -1,5 +1,8 @@
+// The code that uses the generated Vaadin Connect modules:
+
 import client from './src/generated/connect-client.default';
 import {addOne} from './src/generated/DemoVaadinService';
+import {ConnectClient} from '@vaadin/connect';
 
 client.credentials = (options = {}) => {
   return {username: 'test_login', password: 'test_password', stayLoggedIn: true};
@@ -9,3 +12,22 @@ const numberLabel = document.getElementById('number');
 document.getElementById('addOne').onclick = async() => {
   numberLabel.textContent = await addOne(numberLabel.textContent);
 };
+
+// If you don't want to generate the modules and the client, the client npm module itself is enough to work with the Vaadin Connect backend:
+
+const customClient = new ConnectClient({
+  endpoint: '/connect',
+  credentials: (options = {}) => {
+    return {username: 'test_login', password: 'test_password'};
+  }
+});
+
+customClient.call('DemoVaadinService', 'addOne', {
+  number: 5
+}).then(incrementedValue => {
+  if (incrementedValue !== 6) {
+    console.error(`Received unexpected incremented value: ${incrementedValue}`);
+  }
+});
+
+// Or you can handle everything yourself, sending POST requests to Vaadin Connect backend to get tokens and interact with the services.
