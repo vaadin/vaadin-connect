040ecbe1d46af6e38665d73fa632fabe5ba69415
Kirill Bulatov <mail4score@gmail.com>
2018-12-03 17:13:35 +0200

    Add the default client documentation

--- vaadin-connect-demo/frontend/index.js
+++ vaadin-connect-demo/frontend/index.js
@@ -1,8 +1,6 @@
 // The code that uses the generated Vaadin Connect modules:
 import client from './src/generated/connect-client.default';
 import {addOne} from './src/generated/DemoVaadinService';
-// Same code without the generated JavaScript, using vaadin-connect NPM module api instead:
-import {ConnectClient} from '@vaadin/connect';
 
 client.credentials = (options = {}) => {
   return {username: 'test_login', password: 'test_password', stayLoggedIn: true};
@@ -12,6 +10,9 @@ const numberLabel = document.getElementById('number');
 document.getElementById('addOne').addEventListener('click', async() => {
   numberLabel.textContent = await addOne(numberLabel.textContent);
 });
+
+// Same code without the generated JavaScript, using vaadin-connect NPM module api instead:
+import {ConnectClient} from '@vaadin/connect';
 
 const customClient = new ConnectClient({
   endpoint: '/connect',
