09dfef175ec9d7e570bec7a3a2efaad7f6c1db31
Anton Platonov <platosha@gmail.com>
2018-12-10 17:10:10 +0200

    feat(demo): add check anonymous access button and integration test
    Fixes #107

--- vaadin-connect-demo/e2e/demo.spec.js
+++ vaadin-connect-demo/e2e/demo.spec.js
@@ -21,5 +21,38 @@ describe('demo application', () => {
           expect(text).to.equal('2')
         );
     });
+
+    describe('anonymous access', () => {
+      beforeEach(async() => {
+        // TODO: use `client.logout()` here instead
+        await page.execute('localStorage.clear()');
+      });
+
+      it('should allow anonymous access with missing credentials', async() => {
+        // Reload the page in anonymous mode after logout
+        page = page.get('?credentials=none');
+
+        await page
+          .findById('checkAnonymousAccess').click().end()
+          .sleep(5000)
+          .getAlertText().then(text =>
+            expect(text).to.equal('anonymous success')
+          )
+          .dismissAlert();
+      });
+
+      it('should allow anonymous access with wrong credentials', async() => {
+        // Reload the page in anonymous mode after logout
+        page = page.get('?credentials=wrong');
+
+        await page
+          .findById('checkAnonymousAccess').click().end()
+          .sleep(5000)
+          .getAlertText().then(text =>
+            expect(text).to.equal('anonymous success')
+          )
+          .dismissAlert();
+      });
+    });
   });
 });
