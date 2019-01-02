e66a26e0c26d3575165f359c3c4f846b529b84a6
Manolo Carrasco <manolo@apache.org>
2019-01-02 14:21:07 +0100

    feat(prod) Run IT against client bundle. Add bundle to deployable .jar

--- vaadin-connect-demo/e2e/demo.spec.js
+++ vaadin-connect-demo/e2e/demo.spec.js
@@ -23,7 +23,7 @@ describe('demo application', () => {
     it('should say hello after logging in', async() => {
       await page
         .findById('login').click().end()
-        .sleep(5000)
+        .sleep(2000)
         .findById('loginMessage').getVisibleText().then(text =>
           expect(text).to.equal('Hello, test_login!')
         );
@@ -32,7 +32,7 @@ describe('demo application', () => {
     it('should increment number on button click', async() => {
       await page
         .findById('addOne').click().end()
-        .sleep(5000)
+        .sleep(2000)
         .findById('number').getVisibleText().then(text =>
           expect(text).to.equal('2')
         );
@@ -52,7 +52,7 @@ describe('demo application', () => {
             expect(text).to.equal('')
           ).end()
           .findById('checkAnonymousAccess').click().end()
-          .sleep(5000)
+          .sleep(2000)
           .findById('access').getVisibleText().then(text =>
             expect(text).to.equal('anonymous success')
           );
@@ -67,7 +67,7 @@ describe('demo application', () => {
             expect(text).to.equal('')
           ).end()
           .findById('checkAnonymousAccess').click().end()
-          .sleep(5000)
+          .sleep(2000)
           .findById('access').getVisibleText().then(text =>
             expect(text).to.equal('anonymous success')
           );
