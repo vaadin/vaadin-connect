38378fbdce66886ce0eef14503f42a7353402d78
Anton Platonov <platosha@gmail.com>
2019-02-01 16:28:42 +0200

    fix(demo): use full fetch polyfill

--- vaadin-connect-demo/frontend/polyfills.js
+++ vaadin-connect-demo/frontend/polyfills.js
@@ -2,5 +2,5 @@ import URLSearchParams from '@ungap/url-search-params';
 /* global window */
 window.URLSearchParams = URLSearchParams;
 
-import 'unfetch/polyfill/index.js';
+import 'whatwg-fetch';
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch.js';
