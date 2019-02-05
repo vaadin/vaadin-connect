d563488a7088c2a06eefcbf7cd7ae2f08c3ca8e7
Viktor Lukashov <viktor@vaadin.com>
2019-02-05 10:35:16 +0200

    avoid (<any>window) reinterpret casting
    Apparently, there is another way to do that in TypeScript. Subjectively, it looks a bit cleaner.

--- frontend/connect/src/connect-meta.ts
+++ frontend/connect/src/connect-meta.ts
@@ -1,8 +1,12 @@
 import {usageStatistics} from '@vaadin/vaadin-usage-statistics/vaadin-usage-statistics.js';
 
-(<any>window).Vaadin = (<any>window).Vaadin || {};
-(<any>window).Vaadin.registrations = (<any>window).Vaadin.registrations || [];
-(<any>window).Vaadin.registrations.push({
+declare global {
+  interface Window { Vaadin: any; }
+}
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.registrations = window.Vaadin.registrations || [];
+window.Vaadin.registrations.push({
   is: '@vaadin/connect',
   version: '0.4.0',
 });
