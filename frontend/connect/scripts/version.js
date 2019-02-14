bca74c6d0c4d813f63bd2213eb079aba867be39c
Anton Platonov <platosha@gmail.com>
2019-02-14 16:20:18 +0200

    fix(client): stage connect-meta.ts change in version commit (#311)
    * fix(client): stage connect-meta.ts change in version commit

--- frontend/connect/scripts/version.js
+++ frontend/connect/scripts/version.js
@@ -16,3 +16,7 @@ fs.writeFileSync(
     .replace(/version: '.*'/, `version: '${version}'`),
   opts
 );
+
+// Stage this change for the new version tag commit. The same as
+// `$ git add src/connect-meta.ts, but using git plumbing command.
+require('child_process').execSync(`git update-index -- ${metaModulePathname}`);
