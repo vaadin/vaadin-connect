d36ee2796066064c0e7a15fc1a5a5225bcf28596
Tien Nguyen <nqtien.dev@gmail.com>
2019-03-13 09:28:17 +0200

    Use required field in model template

--- vaadin-connect-maven-plugin/src/main/java/com/vaadin/connect/plugin/generator/VaadinConnectTsGenerator.java
+++ vaadin-connect-maven-plugin/src/main/java/com/vaadin/connect/plugin/generator/VaadinConnectTsGenerator.java
@@ -867,8 +867,6 @@ public class VaadinConnectTsGenerator extends AbstractTypeScriptClientCodegen {
     handlebars.registerHelper("multiplelines", getMultipleLinesHelper());
     handlebars.registerHelper("getClassNameFromImports",
         getClassNameFromImportsHelper());
-    handlebars.registerHelper("getOptionalFieldSymbol",
-        getOptionalFieldSymbol());
   }
 
   private Helper<String> getMultipleLinesHelper() {
@@ -881,16 +879,6 @@ public class VaadinConnectTsGenerator extends AbstractTypeScriptClientCodegen {
         buffer.append(options.apply(fn, parent.combine("@line", line)));
       }
       return buffer;
-    };
-  }
-
-  private Helper<String> getOptionalFieldSymbol() {
-    return (context, options) -> {
-      if (StringUtils.endsWith(context, NULLABLE_SUFFIX)) {
-        return "?";
-      } else {
-        return "";
-      }
     };
   }
 
