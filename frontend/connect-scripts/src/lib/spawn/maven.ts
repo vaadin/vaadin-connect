e09c8923fa9d90437f13d330dbfc90ece5837ba5
Artur Signell <artur@vaadin.com>
2019-03-14 13:27:10 +0200

    Make mvnw executable before running it
    Fixes #379

--- frontend/connect-scripts/src/lib/spawn/maven.ts
+++ frontend/connect-scripts/src/lib/spawn/maven.ts
@@ -1,6 +1,13 @@
+import fs = require('fs');
 import {spawn, spawnSync} from '../spawn';
 
 function mavenify<T extends (...args: any[]) => any>(fn: T) {
+  const stat = fs.statSync('./mvnw');
+  // tslint:disable-next-line:no-bitwise
+  if ((stat.mode & fs.constants.S_IXUSR) === 0) {
+    // tslint:disable-next-line:no-bitwise
+    fs.chmodSync('./mvnw', stat.mode | fs.constants.S_IXUSR);
+  }
   return (args: string[]) =>
     fn('./mvnw', ['-e', ...args]) as ReturnType<T>;
 }
